7382cd9925db1b92ff934523657a4a9279d52e91
Sankarshan Mudkavi <sankarshan.mudkavi@textnow.com>
2016-05-18 09:50:31 -0400

    Temp test

--- spec/unit/connection_spec.rb
+++ spec/unit/connection_spec.rb
@@ -11,4 +11,28 @@ describe 'Connection' do
     connection = Devise::LDAP::Connection.new()
     expect(connection.ldap.base).to eq('ou=testbase,dc=test,dc=com')
   end
+
+  describe 'When verifying user groups' do
+    before :all do
+      default_devise_settings!
+      reset_ldap_server!
+    end
+    context 'through ldap_ad_group_check' do
+      it 'should search using user_lookup_attribute and group_lookup_attribute', :focus => true do
+        admin = Factory.create(:admin)
+        #
+        ::Devise.ldap_config = Proc.new() {{
+            'host' => 'localhost',
+            'port' => 3389,
+            'base' => 'ou=testbase,dc=test,dc=com',
+            'attribute' => 'cn',
+            'user_lookup_attribute' => 'mail',
+            'group_lookup_attribute' => 'memberof'
+        }}
+        ::Devise.ldap_ad_group_check = true
+        connection = Devise::LDAP::Connection.new(:login => admin.email, :password => admin.password, :admin => true)
+        connection.in_group? ''
+      end
+    end
+  end
 end
