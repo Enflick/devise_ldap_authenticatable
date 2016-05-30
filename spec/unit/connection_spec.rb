2d4b377404e1c9e7f6944b659d76e775d7d0c992
Sankarshan Mudkavi <sankarshan.mudkavi@textnow.com>
2016-05-30 16:59:16 -0400

    WIP more changes

--- spec/unit/connection_spec.rb
+++ spec/unit/connection_spec.rb
@@ -20,21 +20,23 @@ describe 'Connection' do
     context 'through ldap_ad_group_check' do
       it 'should search using user_lookup_attribute and group_lookup_attribute', :focus => true do
         admin = Factory.create(:admin)
-        #
         ::Devise.ldap_config = Proc.new() {{
             'host' => 'localhost',
             'port' => 3389,
-            'base' => 'ou=people,dc=test,dc=com',
+            'base' => 'ou=testbase,dc=test,dc=com',
+            'group_base' => 'ou=testbase,dc=test,dc=com',
             'attribute' => 'cn',
             'user_lookup_attribute' => 'mail',
             'group_lookup_attribute' => 'memberof',
             'admin_user' => 'cn=admin,dc=test,dc=com',
-            'admin_password' => 'secret'
+            'admin_password' => 'secret',
+            'required_groups' => ['testgroup1', 'testgroup2']
         }}
         ::Devise.ldap_ad_group_check = true
+        ::Devise.ldap_check_group_membership = true
         ::Devise.ldap_auth_username_builder = Proc.new() {|attribute, login, ldap| "#{login}"}
         connection = Devise::LDAP::Connection.new(:login => admin.email, :password => admin.password, :admin => true)
-        assert_equal true, connection.in_group?('mail')
+        assert_equal true, connection.in_group?('cn=admins,ou=testbase,dc=test,dc=com')
       end
     end
   end
